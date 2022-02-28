ebd94a29a60ff96de597e4688814063eea749366
Tito <tito13kfm@gmail.com>
2022-02-27 22:47:37 -0500

    Add project files.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string getFormattedTimeDate()
+{
+    string time = System.DateTime.Now.ToLongTimeString();
+    string date = System.DateTime.Now.ToLongDateString();
+    string dateTime = date + " " + time;
+    string border = new('*', dateTime.Length + 4);
+    string formattedTimeDate = border + "\n| " + dateTime + " | \n" + border;
+    return formattedTimeDate;
+}
+
+var periodicTimer = new PeriodicTimer(TimeSpan.FromSeconds(1));
+while (await periodicTimer.WaitForNextTickAsync())
+{
+    Console.Clear();
+    Console.WriteLine(getFormattedTimeDate());
+}
